c22745b95144178e0e7cc37e240ab3f156fec9b5
Prateek Srivastava <f2prateek@gmail.com>
2015-11-30 08:53:51 +0530

    add instapaper

--- main.go
+++ main.go
@@ -5,10 +5,31 @@ import (
 	"log"
 
 	"github.com/f2prateek/hn-to-instapaper/hn"
+	"github.com/f2prateek/hn-to-instapaper/instapaper"
+	"github.com/tj/docopt"
+)
+
+const (
+	usage = `hn2instapaper.
+
+Usage:
+  hn2instapaper <username> <password>
+  hn2instapaper -h | --help
+  hn2instapaper --version
+
+Options:
+  -h --help     Show this screen.
+  --version     Show version.`
 )
 
 func main() {
+	arguments, err := docopt.Parse(usage, nil, true, "Naval Fate 2.0", false)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	hnClient := hn.New()
+	instapaperClient := instapaper.New(arguments["<username>"].(string), arguments["<password>"].(string))
 
 	stories, err := hnClient.TopStories()
 	if err != nil {
@@ -20,6 +41,15 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Println(i, *story.Title)
+		fmt.Println("Adding", *story.Title)
+
+		result, err := instapaperClient.Add(instapaper.AddParams{
+			URL:   *story.URL,
+			Title: story.Title,
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(i, result.BookmarkID)
 	}
 }
